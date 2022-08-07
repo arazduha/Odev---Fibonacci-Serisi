e8f1f104ae98bf8e8f91ecf0ce53917be6d4f9e4
arazduha <arazduha02@gmail.com>
2022-08-07 20:39:50 +0300

    java101 - odev3.5 - patika.dev

---
+++ src/test3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class test3 {
+    public static  void main (String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("sayi giriniz: ");
+        int sayi = inp.nextInt();
+
+        int n1=0, n2=1, n3, i;
+        System.out.print(n1+" "+n2);
+        for (i = 2; i < sayi; i++){
+            n3 = n1 + n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+
+    }
+
+}
+
+
+
+
+
